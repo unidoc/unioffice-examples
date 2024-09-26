fdbad2676f0a40ef0c35931474a42350e812a7ff
kellem negasi <kellemnegasi@gmail.com>
2024-09-26 20:42:16 +0300

    updated example code and README file

--- license/usage-logs/README.md
+++ license/usage-logs/README.md
@@ -1,16 +1,20 @@
 ### Metered License Key Usage Logs
-When Using unioffice it is possible to see the usage logs of license key in every run. To enable this behavior all we have to do is setting the `UsageLog` to verbose and the log level to `Info` or above as follows.
+When using unioffice on metered api key it is possible to see the usage logs of license key in every run. To enable this behavior all we have to do is setting the `UsageLog` to verbose and the log level to `Info` or above as follows. This is available for metered api key only.
 
 ```go
 // Set the log level to info or higher
-	logger.SetLogger(logger.NewConsoleLogger(logger.LogLevelInfo))
+logger.SetLogger(logger.NewConsoleLogger(logger.LogLevelInfo))
 
 // Enable the verbose mode logging
 license.SetMeteredKeyUsageLogVerboseMode(true)
 ```
 Sample output 
-![alt text](sample_output.png)
+```bash
+[INFO]  metered.go:674 2024-09-26 20:32:22.607531 +0300 +03 m=+0.412134293 | File grocery_list.docx | Ref: dr17f8db | document.Read | 1 credit(s) used
+[INFO]  metered.go:506 2024-09-26 20:32:22.653044 +0300 +03 m=+0.457648876 Ref: dr17f8db | document:d.Save | No credit used
+```
+
 #### Examples 
-- [Usage Logs for Docx to PDF Conversion](main.go) This example shows how to display license usage logs for docx to pdf conversion process.
+- [Modify docx file](main.go) This example shows how to display license usage logs for a editing simple docx file.
 
 
--- license/usage-logs/main.go
+++ license/usage-logs/main.go
@@ -1,43 +1,19 @@
-/*
- * This example showcases how to see license usage logs
- * in docx to PDF conversing with UniOffice.
- */
-
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
-	"path"
-	"path/filepath"
-	"strings"
-
-	unipdflicense "github.com/unidoc/unipdf/v3/common/license"
 
 	"github.com/unidoc/unioffice/common/license"
 	"github.com/unidoc/unioffice/common/logger"
 	"github.com/unidoc/unioffice/document"
-	"github.com/unidoc/unioffice/document/convert"
 )
 
 func init() {
 	// Make sure to load your metered License API key prior to using the library.
 	// If you need a key, you can sign up and create a free one at https://cloud.unidoc.io
-	err := unipdflicense.SetMeteredKey(os.Getenv(`UNIDOC_LICENSE_API_KEY`))
+	err := license.SetMeteredKey(os.Getenv(`UNIDOC_LICENSE_API_KEY`))
 	if err != nil {
-		fmt.Printf("ERROR: Failed to set metered key: %v\n", err)
-		fmt.Printf("Make sure to get a valid key from https://cloud.unidoc.io\n")
-		fmt.Printf("If you don't have one - Grab one in the Free Tier at https://cloud.unidoc.io\n")
-		panic(err)
-	}
-
-	// This example requires both for unioffice and unipdf.
-	err = license.SetMeteredKey(os.Getenv(`UNIDOC_LICENSE_API_KEY`))
-	if err != nil {
-		fmt.Printf("ERROR: Failed to set metered key: %v\n", err)
-		fmt.Printf("Make sure to get a valid key from https://cloud.unidoc.io\n")
-		fmt.Printf("If you don't have one - Grab one in the Free Tier at https://cloud.unidoc.io\n")
 		panic(err)
 	}
 
@@ -49,33 +25,28 @@ func init() {
 }
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Printf("Usage: go run unipdf_license_usage_log.go input_dir output_dir\n")
-		os.Exit(1)
-	}
-
-	inputDir := os.Args[1]
-	output_dir := os.Args[2]
-	files, err := filepath.Glob(inputDir + "*.docx")
+	additional_items := []string{"Yogurt", "Cheese", "Cereal", "Pasta", "Olive Oil"}
+	doc, err := document.Open("grocery_list.docx")
 	if err != nil {
-		log.Fatalf("failed to get files from directory %s %s", inputDir, err)
+		log.Fatalf("error opening document: %s", err)
+	}
+	defer doc.Close()
+
+	var itemStyle string
+
+	// get the style of the list item
+	paras := doc.Paragraphs()
+	if len(paras) > 1 {
+		p := paras[1]
+		itemStyle = p.Style()
 	}
 
-	for _, filePath := range files {
-		base := path.Base(filePath)
-		name := strings.TrimSuffix(base, filepath.Ext(base))
-		outputPath := fmt.Sprintf("%s/%s.pdf", output_dir, name)
-		doc, err := document.Open(filePath)
-		if err != nil {
-			log.Fatalf("error opening document: %s", err)
-		}
-		defer doc.Close()
-
-		c := convert.ConvertToPdf(doc)
-
-		err = c.WriteToFile(outputPath)
-		if err != nil {
-			log.Fatalf("error converting document: %s", err)
-		}
+	for _, item := range additional_items {
+		newP := doc.AddParagraph()
+		r := newP.AddRun()
+		r.AddText(item)
+		newP.SetStyle(itemStyle)
 	}
+
+	doc.SaveToFile("updated_list.docx")
 }
