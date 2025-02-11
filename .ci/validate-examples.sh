b5f64d2912bfc6f365b4882fbe04ede89e77d479
Alip <alipxsulistio@gmail.com>
2025-02-12 02:24:35 +0700

    update path

--- .ci/validate-examples.sh
+++ .ci/validate-examples.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 :> errors
-output=$(find $PWD -maxdepth 2 -mindepth 1 -not -path '*/\.*' -type d ! -name "metered" ! -name "offline" ! -name "metered-non-persistent-cache" ! -name "usage-logs" -print0 | xargs -0 -I{} sh -c "cd {}; echo running {}; ./main")
+output=$(find ../ -maxdepth 2 -mindepth 1 -not -path '*/\.*' -type d ! -name "metered" ! -name "offline" ! -name "metered-non-persistent-cache" ! -name "usage-logs" -print0 | xargs -0 -I{} sh -c "cd {}; echo running {}; ./main")
 if [ $? -ne 0 ]; then
     echo $output >> errors
 fi
 export PATH=$PATH:$HOME/dotnet
-find $PWD -name "*.docx" $(printf "! -name %s " $(cat .ci/skip_files)) -exec .ci/dotnet_run.sh {} docx \;
-find $PWD -name "*.xlsx" $(printf "! -name %s " $(cat .ci/skip_files)) -exec .ci/dotnet_run.sh {} xlsx \;
-find $PWD -name "*.pptx" $(printf "! -name %s " $(cat .ci/skip_files)) -exec .ci/dotnet_run.sh {} pptx \;
+find ../ -name "*.docx" $(printf "! -name %s " $(cat skip_files)) -exec ./dotnet_run.sh {} docx \;
+find ../ -name "*.xlsx" $(printf "! -name %s " $(cat skip_files)) -exec ./dotnet_run.sh {} xlsx \;
+find ../ -name "*.pptx" $(printf "! -name %s " $(cat skip_files)) -exec ./dotnet_run.sh {} pptx \;
 echo Errors: $(wc -l errors)
 if [[ $(wc -l errors | awk '{print $1}') == "0" ]]; then
 	exit 0
