9a442dee34fb24e88bf9d95fb1d3bc1f5806c25c
Alip <alipxsulistio@gmail.com>
2025-02-12 23:33:34 +0700

    add go mod tidy

--- .ci/build-examples.sh
+++ .ci/build-examples.sh
@@ -2,6 +2,7 @@
 
 cd $PWD
 go get github.com/go-ole/go-ole/oleutil@v1.3.0
+go mod tidy
 
 :> build_errors
 find $PWD -maxdepth 2 -mindepth 1 -not -path '*/\.*' -type d -exec sh -c "cd {}; echo building {}; go build -tags=test main.go" 2>>build_log \;
